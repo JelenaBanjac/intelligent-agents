7970a8ca60a424c2d3b17f461c0284dc613c06e6
Jelena Banjac <jelena.b94@gmail.com>
2019-11-24 15:43:35 +0100

    add fixed simple agent

--- 5-auction/banjac-triantafyllou-ex5/src/template/SimpleAuctionAgent.java
+++ 5-auction/banjac-triantafyllou-ex5/src/template/SimpleAuctionAgent.java
@@ -2,7 +2,9 @@ package template;
 
 //the list of imports
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 import java.io.File;
@@ -26,7 +28,8 @@ import variables.SolutionCost;
 
 @SuppressWarnings("unused")
 public class SimpleAuctionAgent implements AuctionBehavior {
-
+	private boolean debug = false;
+	
 	private Topology topology;
 	private TaskDistribution distribution;
 	private Agent agent;
@@ -117,6 +120,8 @@ public class SimpleAuctionAgent implements AuctionBehavior {
 		double currentCost = cost(solution);
 		
 		Solution extendedSolution = Solution.extendSolution(solution, currentTask);
+		extendedSolution = new Solution(getSolution(extendedSolution.getVehicles(), extendedSolution.getTasks(), timeout_bid/2));
+		
 		double extendedCost = cost(extendedSolution);
 		
 		double marginalCost = Math.max(0, extendedCost - currentCost);
@@ -153,18 +158,73 @@ public class SimpleAuctionAgent implements AuctionBehavior {
 		return bid;
 	}
 	
-	public Solution getSolution(List<Vehicle> vehicles, TaskSet tasks) {
-		// TODO: change this
-		Solution solution = new Solution(vehicles, tasks);
+	public Solution getSolution(List<Vehicle> vehicles, List<Task> tasks, long timeout) {
+		// select initial solution
+    	Solution A = new Solution(vehicles, tasks);
+    	System.out.println("...Computing SLS for "+ A.getTasks().size() +" tasks...");
 		
-		return solution;
+    	if (A.getTasks().size() <= 1) {
+    		return A;
+    	}
+    	
+    	int iteration = 0;
+    	int maxNumberOfIterations = 1000000;
+    	double p = 0.1;  // best [0.3, 0.5]
+    	
+    	long start_time = System.currentTimeMillis();
+    	long current_time = System.currentTimeMillis();
+    	
+    	if (debug) System.out.println("Iteration " + iteration + " (" + (current_time-start_time) + "ms) cost " + cost(A));
+		
+		HashSet<Solution> rollbackSolutions = new HashSet<Solution>();
+		// add this solution just so it is not empty 
+		rollbackSolutions.add(A);
+		
+		int sameSolution = 0;
+		int sameSolutionLimit = 10;
+    	
+    	do {
+			Solution Aold = new Solution(A);
+			List<Solution> N = chooseNeighbors(Aold);
+			A = localChoice(N, A, p);
+			
+			current_time = System.currentTimeMillis();
+			
+    		iteration++;
+    		
+    		if (cost(Aold) > cost(A)) {
+				if (debug) System.out.println("Iteration " + iteration + " (" + (current_time-start_time) + "ms) cost " + cost(A));
+	    		
+	    		rollbackSolutions.add(A);
+	    		sameSolution = 0;
+			} else {
+				sameSolution++;
+				if (sameSolution > sameSolutionLimit) {
+					int randInt = random.nextInt(10);
+					A = getRandomObject(rollbackSolutions);
+					p = random.nextFloat();
+					if (debug) System.out.println("--- rollback --- (solution cost )" + cost(A));
+				}
+			}
+		} while (iteration < maxNumberOfIterations && (current_time-start_time + 1000) < timeout);
+    	
+    	
+    	A = getBestSolution(rollbackSolutions);
+    	
+    	System.out.println("\t[SLS] Number of iterations " + iteration);
+    	System.out.println("\t[SLS] Solution cost " + cost(A));
+    	return A;
 	}
 	
     @Override
     public List<Plan> plan(List<Vehicle> vehicles, TaskSet tasks) {
         long time_start = System.currentTimeMillis();
         
-        Solution solution = getSolution(vehicles, tasks);
+        ArrayList<Task> tsks = new ArrayList<Task>();
+        tsks.addAll(tasks);
+        
+        Solution solution = getSolution(vehicles, tsks, timeout_plan);
+        
         double totalCost = cost(solution);
         
         if (totalCost > totalReward) {
@@ -217,4 +277,153 @@ public class SimpleAuctionAgent implements AuctionBehavior {
         }
         return plan;
     }
+	
+	//************************************************** For SLS **************************************************//
+	
+	
+	private List<Solution> chooseNeighbors(Solution Aold) {
+		/**
+		 * In each iteration, we choose one vehicle at random and perform 
+		 * local operators on this vehicle to compute the neighbor solutions.
+		 */
+		List<Solution> N = new ArrayList<Solution>();
+
+		for (Vehicle vi : Aold.getVehicles()) {
+		
+			// apply the changing vehicle operator
+			for (Vehicle vj : Aold.getVehicles()) {
+				if (vi == vj || Aold.variables.get(vj).size() == 0) continue;
+				
+				Solution Anew = changeVehicle(Aold, vj, vi);
+				if (Anew.constraints()) N.add(Anew);
+				
+				// apply the changing task order operator
+				int numTasks = Aold.variables.get(vi).size();
+				if (numTasks >= 4) {
+					for (int tIdx1 = 0; tIdx1 < numTasks-1; tIdx1++) {
+						for (int tIdx2 = tIdx1+1; tIdx2 < numTasks; tIdx2++) {
+							PDTask t1 = Aold.variables.get(vi).get(tIdx1);
+							PDTask t2 = Aold.variables.get(vi).get(tIdx2);
+							
+							Solution A = changingTaskOrder(Aold, vi, tIdx1, tIdx2);
+							
+							if (A.constraints()) N.add(A);
+						}
+					}
+				}
+				// end
+				
+			}
+
+			// apply the changing task order operator
+			int numberOfTasksInVehicle = Aold.variables.get(vi).size();
+			if (numberOfTasksInVehicle >= 4) {
+				for (int tIdx1 = 0; tIdx1 < numberOfTasksInVehicle-1; tIdx1++) {
+					for (int tIdx2 = tIdx1+1; tIdx2 < numberOfTasksInVehicle; tIdx2++) {
+						PDTask t1 = Aold.variables.get(vi).get(tIdx1);
+						PDTask t2 = Aold.variables.get(vi).get(tIdx2);
+						
+						Solution Anew = changingTaskOrder(Aold, vi, tIdx1, tIdx2);
+						if (Anew.constraints()) N.add(Anew);
+						
+						// apply the changing vehicle operator
+						for (Vehicle vj : Anew.getVehicles()) {
+							if (vi == vj || Anew.variables.get(vj).size() == 0) continue;
+							
+							Solution A = changeVehicle(Anew, vj, vi);
+						
+							if (A.constraints()) N.add(A);
+						}
+						// end
+							
+					}
+				}
+			}	
+			
+		}
+			
+		return N;
+	}
+	
+	private Solution changeVehicle(Solution A, Vehicle v1, Vehicle v2) {
+    	/**
+    	 * Operator 1: Take the first task from the tasks of one vehicle 
+    	 * and give it to another vehicle.
+    	 */
+    	Solution A1 = new Solution(A);
+    	
+    	List<PDTask> v1Tasks = new ArrayList<PDTask>(A1.variables.get(v1));
+    	List<PDTask> v2Tasks = new ArrayList<PDTask>(A1.variables.get(v2));
+    	
+    	int idx = 0; //random.nextInt(A.variables.keySet().size()-1);
+    	
+    	PDTask tP = A1.variables.get(v1).get(idx); 
+    	PDTask tD = A1.findPairTask(v1, tP); 
+    	v1Tasks.remove(tP);
+    	v1Tasks.remove(tD);
+    	
+    	// add both pickup and delivery to beginning of another vehicle tasks
+    	v2Tasks.add(0, tD);  // v2Tasks.add(tP);
+    	v2Tasks.add(0, tP);  //v2Tasks.add(tD);
+    	
+    	A1.variables.put(v1, v1Tasks);
+    	A1.variables.put(v2, v2Tasks);
+    	
+    	return A1;
+    }
+    
+    private Solution changingTaskOrder(Solution A, Vehicle vi, int tIdx1, int tIdx2) {
+    	/**
+    	 * Operator 2: Change the order of two tasks in the task list of 
+    	 * a vehicle.
+    	 */
+    	Solution A1 = new Solution(A);
+    	
+    	List<PDTask> vTasks = new ArrayList<PDTask>(A.variables.get(vi));
+    	
+    	Collections.swap(vTasks, tIdx1, tIdx2);
+    	
+    	A1.variables.put(vi, vTasks);
+    	
+    	return A1;
+    }
+    
+    private Solution localChoice(List<Solution> N, Solution A, double p) {
+    	Solution Anew = A;
+    	double bestCost = cost(A);
+    	
+    	if (Anew != null && random.nextFloat() < p) {
+	    	for (Solution s : N) {
+	    		double cost = cost(s);
+	    		if (cost <= bestCost) {
+	    			Anew = s;
+	    			bestCost = cost;
+	    		}
+	    	}
+	    	return Anew;
+    	} else {
+    		int randInd = random.nextInt(N.size());
+    		return N.get(randInd);
+    	}
+    	
+    }
+    
+    public Solution getBestSolution(HashSet<Solution> solutions) {
+    	double minCost = 10000000.0;
+    	Solution bestSolution = null;
+    	for (Solution s : solutions) {
+    		if (cost(s) < minCost) {
+    			minCost = cost(s);
+    			bestSolution = s;
+    		}
+    	}
+    	return bestSolution;
+    }
+    
+    private Solution getRandomObject(HashSet<Solution> from) {
+ 	   Random rnd = new Random();
+ 	   int i = rnd.nextInt(from.size());
+ 	   return (Solution) from.toArray()[i];
+ 	}
 }
+
